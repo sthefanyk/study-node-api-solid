fabddff6dfebc6e1a1d88a658c2070faa344db0e
sthefanyk <sthefanykmbrl@gmail.com>
2024-09-11 16:17:30 -0300

    :wrench: Configura ORM e banco de dados
    Configura integração do prisma com o banco de dados postgreSQL.
    
    - Adiciona prisma e @prisma/client como dependencias.
    - Configura arquivo docker-compose para subir o postgreSQL.
    - Adiciona DATABASE_URL como variavel de ambiente.
    - Cria schemas no prisma (tabelas e seus relacionamentos).
    - Cria migrations com prima.
    - Adiciona arquivo para logs do prisma em ambiente de desenvolvimento.
    - Testa configuraçãoes feitas criando rota para testar criação do user.

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,26 @@
 import fastify from 'fastify'
+import { z } from 'zod'
+import { prisma } from './lib/prisma'
 
 export const app = fastify()
+
+app.post('/users', async (request, reply) => {
+    const userSchema = z.object({
+        name: z.string(),
+        email: z.string().email(),
+        password: z.string().min(6),
+    })
+
+    const { name, email, password } = userSchema.parse(request.body)
+    const passwordHash = password
+
+    await prisma.user.create({
+        data: {
+            name,
+            email,
+            password_hash: passwordHash,
+        },
+    })
+
+    return reply.status(201).send()
+})
