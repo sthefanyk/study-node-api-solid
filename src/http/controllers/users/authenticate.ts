f5600e06f6f8aac7947367aff9f93b54280aa8ee
sthefanyk <sthefanykmbrl@gmail.com>
2024-09-13 17:49:35 -0300

    :sparkles: Implementa e testa rota de refresh token
    - Adiciona um tempo de expiração de 10 minutos para o token após a autenticação.
    - Modifica a implementação de autenticação para que salve um token de expiração
    	de 7 dias nos cookie, que servira como refresh token.
    - Implementa rota de refresh token.
    - Realiza testes e2e para a rota de refresh token

--- src/http/controllers/users/authenticate.ts
+++ src/http/controllers/users/authenticate.ts
@@ -29,9 +29,25 @@ export async function authenticate(
             },
         )
 
-        return reply.status(200).send({
-            token,
-        })
+        const refreshToken = await reply.jwtSign(
+            {},
+            {
+                sub: user.id,
+                expiresIn: '7d',
+            },
+        )
+
+        return reply
+            .status(200)
+            .setCookie('refreshToken', refreshToken, {
+                path: '/',
+                secure: true,
+                sameSite: true,
+                httpOnly: true,
+            })
+            .send({
+                token,
+            })
     } catch (err) {
         if (err instanceof InavalidCredentialsError) {
             return reply.status(400).send({ message: err.message })
--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,10 +3,13 @@ import { regiter } from './register'
 import { authenticate } from './authenticate'
 import { profile } from './profile'
 import { verfifyJWT } from '../../middlewares/verify-jwt'
+import { refresh } from './refresh'
 
 export async function usersRoutes(app: FastifyInstance) {
     app.post('/users', regiter)
     app.post('/sessions', authenticate)
+
+    app.patch('/token/refresh', refresh)
 
     /** Authenticated */
     app.get('/me', { onRequest: [verfifyJWT] }, profile)
